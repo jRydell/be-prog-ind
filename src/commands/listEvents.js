ca3cc05a99da622af12aa0a1d43d9fc1bdc3dabf
Johan Rydell <johannilsrydell89@gmail.com>
2024-11-04 16:33:55 +0100

    Update API_URL in .env for local development and update error handling in listEvents. not working..

--- src/commands/listEvents.js
+++ src/commands/listEvents.js
@@ -2,10 +2,22 @@ const axios = require("axios");
 
 async function listEvents() {
   try {
-    const response = await axios.get(`${process.env.API_URL}/events`);
+    const response = await axios.get(`${process.env.API_URL}/events`, {
+      headers: {
+        Authorization: `Bearer ${process.env.TOKEN_SECRET}`,
+      },
+    });
     console.log("Public Events:", response.data);
   } catch (error) {
-    console.error("Error fetching events:", error.message);
+    if (error.response) {
+      console.error(
+        `Error: ${error.response.status} - ${JSON.stringify(
+          error.response.data
+        )}`
+      );
+    } else {
+      console.error("Error fetching events:", error.message);
+    }
   }
 }
 
