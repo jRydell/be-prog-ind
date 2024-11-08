a7ae30dd0cea20e37beec3905a30e5eb2c24d67d
Johan Rydell <johannilsrydell89@gmail.com>
2024-11-08 10:21:33 +0100

    .

--- src/prompts/loginPrompt.js
+++ src/prompts/loginPrompt.js
@@ -10,7 +10,7 @@ async function loginPrompt() {
     {
       type: "password",
       name: "password",
-      message: "Enter your password",
+      message: "Enter your password:",
       mask: "*",
     },
   ];
